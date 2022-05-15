46186358a69a3598e52cb58d4b3d682be6a0123b
EnesSERENLI <enes.serenli@hotmail.com>
2022-05-15 12:44:47 +0300

    DbFirst - LinQ and LinQToEntity Queries

---
+++ DbFirst_Queries/DbFirst_Queries/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace DbFirst_Queries
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSorgu12 = new System.Windows.Forms.Button();
+            this.btnSorgu6 = new System.Windows.Forms.Button();
+            this.btnSorgu11 = new System.Windows.Forms.Button();
+            this.btnSorgu10 = new System.Windows.Forms.Button();
+            this.btnSorgu5 = new System.Windows.Forms.Button();
+            this.btnSorgu9 = new System.Windows.Forms.Button();
+            this.btnSorgu14 = new System.Windows.Forms.Button();
+            this.btnSorgu4 = new System.Windows.Forms.Button();
+            this.btnSorgu8 = new System.Windows.Forms.Button();
+            this.btnSorgu13 = new System.Windows.Forms.Button();
+            this.btnSorgu3 = new System.Windows.Forms.Button();
+            this.btnSorgu7 = new System.Windows.Forms.Button();
+            this.btnSorgu2 = new System.Windows.Forms.Button();
+            this.btnSorguBir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(814, 186);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(324, 290);
+            this.listBox1.TabIndex = 18;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(32, 186);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 292);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // btnSorgu12
+            // 
+            this.btnSorgu12.Location = new System.Drawing.Point(617, 82);
+            this.btnSorgu12.Name = "btnSorgu12";
+            this.btnSorgu12.Size = new System.Drawing.Size(111, 34);
+            this.btnSorgu12.TabIndex = 3;
+            this.btnSorgu12.Text = "Sorgu Oniki";
+            this.btnSorgu12.UseVisualStyleBackColor = true;
+            this.btnSorgu12.Click += new System.EventHandler(this.btnSorgu12_Click);
+            // 
+            // btnSorgu6
+            // 
+            this.btnSorgu6.Location = new System.Drawing.Point(617, 42);
+            this.btnSorgu6.Name = "btnSorgu6";
+            this.btnSorgu6.Size = new System.Drawing.Size(111, 34);
+            this.btnSorgu6.TabIndex = 4;
+            this.btnSorgu6.Text = "Sorgu Altı";
+            this.btnSorgu6.UseVisualStyleBackColor = true;
+            this.btnSorgu6.Click += new System.EventHandler(this.btnSorgu6_Click);
+            // 
+            // btnSorgu11
+            // 
+            this.btnSorgu11.Location = new System.Drawing.Point(500, 82);
+            this.btnSorgu11.Name = "btnSorgu11";
+            this.btnSorgu11.Size = new System.Drawing.Size(111, 34);
+            this.btnSorgu11.TabIndex = 5;
+            this.btnSorgu11.Text = "Sorgu Onbir";
+            this.btnSorgu11.UseVisualStyleBackColor = true;
+            this.btnSorgu11.Click += new System.EventHandler(this.btnSorgu11_Click);
+            // 
+            // btnSorgu10
+            // 
+            this.btnSorgu10.Location = new System.Drawing.Point(383, 82);
+            this.btnSorgu10.Name = "btnSorgu10";
+            this.btnSorgu10.Size = new System.Drawing.Size(111, 34);
+            this.btnSorgu10.TabIndex = 6;
+            this.btnSorgu10.Text = "Sorgu On";
+            this.btnSorgu10.UseVisualStyleBackColor = true;
+            this.btnSorgu10.Click += new System.EventHandler(this.btnSorgu10_Click);
+            // 
+            // btnSorgu5
+            // 
+            this.btnSorgu5.Location = new System.Drawing.Point(500, 42);
+            this.btnSorgu5.Name = "btnSorgu5";
+            this.btnSorgu5.Size = new System.Drawing.Size(111, 34);
+            this.btnSorgu5.TabIndex = 7;
+            this.btnSorgu5.Text = "Sorgu Beş";
+            this.btnSorgu5.UseVisualStyleBackColor = true;
+            this.btnSorgu5.Click += new System.EventHandler(this.btnSorgu5_Click);
+            // 
+            // btnSorgu9
+            // 
+            this.btnSorgu9.Location = new System.Drawing.Point(266, 82);
+            this.btnSorgu9.Name = "btnSorgu9";
+            this.btnSorgu9.Size = new System.Drawing.Size(111, 34);
+            this.btnSorgu9.TabIndex = 8;
+            this.btnSorgu9.Text = "Sorgu Dokuz";
+            this.btnSorgu9.UseVisualStyleBackColor = true;
+            this.btnSorgu9.Click += new System.EventHandler(this.btnSorgu9_Click);
+            // 
+            // btnSorgu14
+            // 
+            this.btnSorgu14.Location = new System.Drawing.Point(149, 122);
+            this.btnSorgu14.Name = "btnSorgu14";
+            this.btnSorgu14.Size = new System.Drawing.Size(111, 34);
+            this.btnSorgu14.TabIndex = 9;
+            this.btnSorgu14.Text = "Sorgu On dört";
+            this.btnSorgu14.UseVisualStyleBackColor = true;
+            this.btnSorgu14.Click += new System.EventHandler(this.btnSorgu14_Click);
+            // 
+            // btnSorgu4
+            // 
+            this.btnSorgu4.Location = new System.Drawing.Point(383, 42);
+            this.btnSorgu4.Name = "btnSorgu4";
+            this.btnSorgu4.Size = new System.Drawing.Size(111, 34);
+            this.btnSorgu4.TabIndex = 10;
+            this.btnSorgu4.Text = "Sorgu Dört";
+            this.btnSorgu4.UseVisualStyleBackColor = true;
+            this.btnSorgu4.Click += new System.EventHandler(this.btnSorgu4_Click);
+            // 
+            // btnSorgu8
+            // 
+            this.btnSorgu8.Location = new System.Drawing.Point(149, 82);
+            this.btnSorgu8.Name = "btnSorgu8";
+            this.btnSorgu8.Size = new System.Drawing.Size(111, 34);
+            this.btnSorgu8.TabIndex = 11;
+            this.btnSorgu8.Text = "Sorgu Sekiz";
+            this.btnSorgu8.UseVisualStyleBackColor = true;
+            this.btnSorgu8.Click += new System.EventHandler(this.btnSorgu8_Click);
+            // 
+            // btnSorgu13
+            // 
+            this.btnSorgu13.Location = new System.Drawing.Point(32, 122);
+            this.btnSorgu13.Name = "btnSorgu13";
+            this.btnSorgu13.Size = new System.Drawing.Size(111, 34);
+            this.btnSorgu13.TabIndex = 12;
+            this.btnSorgu13.Text = "Sorgu Onüç";
+            this.btnSorgu13.UseVisualStyleBackColor = true;
+            this.btnSorgu13.Click += new System.EventHandler(this.btnSorgu13_Click);
+            // 
+            // btnSorgu3
+            // 
+            this.btnSorgu3.Location = new System.Drawing.Point(266, 42);
+            this.btnSorgu3.Name = "btnSorgu3";
+            this.btnSorgu3.Size = new System.Drawing.Size(111, 34);
+            this.btnSorgu3.TabIndex = 13;
+            this.btnSorgu3.Text = "Sorgu Üç";
+            this.btnSorgu3.UseVisualStyleBackColor = true;
+            this.btnSorgu3.Click += new System.EventHandler(this.btnSorgu3_Click);
+            // 
+            // btnSorgu7
+            // 
+            this.btnSorgu7.Location = new System.Drawing.Point(32, 82);
+            this.btnSorgu7.Name = "btnSorgu7";
+            this.btnSorgu7.Size = new System.Drawing.Size(111, 34);
+            this.btnSorgu7.TabIndex = 14;
+            this.btnSorgu7.Text = "Sorgu Yedi";
+            this.btnSorgu7.UseVisualStyleBackColor = true;
+            this.btnSorgu7.Click += new System.EventHandler(this.btnSorgu7_Click);
+            // 
+            // btnSorgu2
+            // 
+            this.btnSorgu2.Location = new System.Drawing.Point(149, 42);
+            this.btnSorgu2.Name = "btnSorgu2";
+            this.btnSorgu2.Size = new System.Drawing.Size(111, 34);
+            this.btnSorgu2.TabIndex = 15;
+            this.btnSorgu2.Text = "Sorgu İki";
+            this.btnSorgu2.UseVisualStyleBackColor = true;
+            this.btnSorgu2.Click += new System.EventHandler(this.btnSorgu2_Click);
+            // 
+            // btnSorguBir
+            // 
+            this.btnSorguBir.Location = new System.Drawing.Point(32, 42);
+            this.btnSorguBir.Name = "btnSorguBir";
+            this.btnSorguBir.Size = new System.Drawing.Size(111, 34);
+            this.btnSorguBir.TabIndex = 16;
+            this.btnSorguBir.Text = "Sorgu Bir";
+            this.btnSorguBir.UseVisualStyleBackColor = true;
+            this.btnSorguBir.Click += new System.EventHandler(this.btnSorguBir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1173, 528);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSorgu12);
+            this.Controls.Add(this.btnSorgu6);
+            this.Controls.Add(this.btnSorgu11);
+            this.Controls.Add(this.btnSorgu10);
+            this.Controls.Add(this.btnSorgu5);
+            this.Controls.Add(this.btnSorgu9);
+            this.Controls.Add(this.btnSorgu14);
+            this.Controls.Add(this.btnSorgu4);
+            this.Controls.Add(this.btnSorgu8);
+            this.Controls.Add(this.btnSorgu13);
+            this.Controls.Add(this.btnSorgu3);
+            this.Controls.Add(this.btnSorgu7);
+            this.Controls.Add(this.btnSorgu2);
+            this.Controls.Add(this.btnSorguBir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSorgu12;
+        private System.Windows.Forms.Button btnSorgu6;
+        private System.Windows.Forms.Button btnSorgu11;
+        private System.Windows.Forms.Button btnSorgu10;
+        private System.Windows.Forms.Button btnSorgu5;
+        private System.Windows.Forms.Button btnSorgu9;
+        private System.Windows.Forms.Button btnSorgu14;
+        private System.Windows.Forms.Button btnSorgu4;
+        private System.Windows.Forms.Button btnSorgu8;
+        private System.Windows.Forms.Button btnSorgu13;
+        private System.Windows.Forms.Button btnSorgu3;
+        private System.Windows.Forms.Button btnSorgu7;
+        private System.Windows.Forms.Button btnSorgu2;
+        private System.Windows.Forms.Button btnSorguBir;
+    }
+}
+
---
+++ DbFirst_Queries/DbFirst_Queries/Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DbFirst_Queries
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        NorthwindEntities db = new NorthwindEntities();
+
+        //Entityframework
+        //ORM (Object Relational MApping) bağlantı isteğiyle beraber entityframework ile tabloları class'lara dönüştürür
+
+        private void btnSorguBir_Click(object sender, EventArgs e)
+        {
+            //Soru
+
+            //Fiyatı 20 ile 50 arasında olan ürünlerin ID,Ürünadı,fiyatı,stok miktarı ve kategori adını listeleyin.
+            //T-SQL
+            /*
+             select ProductID,ProductName,UnitPrice,UnitsInStock,CategoryName as 'CategoryName' from Products p join Categories c on c.CategoryID=p.CategoryID Where UnitPrice>20 and UnitPrice<50
+             */
+
+            //Linq
+            //var result = from p in db.Products //bana p gelicek productstan
+            //             where p.UnitPrice > 20 && p.UnitPrice < 50 //filtreledik
+            //             select new
+            //             {
+            //                 p.ProductID,
+            //                 p.ProductName,
+            //                 p.UnitPrice,
+            //                 p.UnitsInStock,
+            //                 p.Category.CategoryName
+            //             };
+            //dataGridView1.DataSource = result.ToList();
+
+            //Linq to Entity
+            var result = db.Products.Where(x => x.UnitPrice > 20 && x.UnitPrice < 50).Select(x => new //Lambda ile yapılış x=>x.property --ilk x products olarak algılıyor.
+            {
+                x.ProductID,
+                x.ProductName,
+                x.UnitPrice,
+                x.UnitsInStock,
+                x.Category.CategoryName
+            }).ToList();
+            dataGridView1.DataSource = result;
+        }
+
+        private void btnSorgu2_Click(object sender, EventArgs e)
+        {
+            //Soru
+            /*
+             * Siparişler tablosundan müşteri şirket adı,çalışan adı ve soyadı, sipariş ID'si,Sipariş tarihi ve KargoŞirket adını getiren sorgu yazın
+             */
+
+            //T-SQL
+
+            //select o.OrderID,e.FirstName+' '+e.LastName as 'Çalışan Bilgisi',c.CompanyName as 'Müşteri Şirket',o.OrderDate,s.CompanyName as 'Kargo Şirket' from Orders o join Employees e on e.EmployeeID=o.EmployeeID join Shippers s on s.ShipperID=o.ShipVia join Customers c on c.CustomerID=o.CustomerID
+
+            //Linq
+
+            //var result = from o in db.Orders
+            //             select new
+            //             {
+            //                 o.OrderID,
+            //                 AdSoyad=o.Employee.FirstName+" "+o.Employee.LastName,
+            //                 MüşteriAd=o.Customer.CompanyName,
+            //                 o.OrderDate,
+            //                 o.Shipper.CompanyName
+            //             };
+            //dataGridView1.DataSource = result.ToList();
+            //Lint to Entity
+
+            var result = db.Orders.Select(o => new
+            {
+                o.OrderID,
+                AdSoyad = o.Employee.FirstName + " " + o.Employee.LastName,
+                MüşteriAd = o.Customer.CompanyName,
+                o.OrderDate,
+                o.Shipper.CompanyName //yukarıda da company name var ona isim vermezsek burada kızar.. Hangi companyname oldugunu bilmesi gerekiyor.
+            }).ToList();
+            dataGridView1.DataSource = result;
+        }
+
+        private void btnSorgu3_Click(object sender, EventArgs e)
+        {
+            //Müşteri tablosunda bulunan 'CompanyName' içerisinde "restaurant"geçen müşterileri listeleyin
+
+            //T-SQL
+            //select * from Customers where CompanyName like '%restaurant%'
+
+            //Linq
+
+            //var result = from c in db.Customers
+            //             where c.CompanyName.Contains("restaurant")
+            //             select c;
+            //dataGridView1.DataSource = result.ToList();
+
+            //Linq to Entity
+
+            var result2 = db.Customers.Where(x => x.CompanyName.Contains("restaurant")).ToList();
+            dataGridView1.DataSource = result2;
+        }
+
+        private void btnSorgu4_Click(object sender, EventArgs e)
+        {
+            //kategorisi beverages olan ve ürün adı "kola" fiyatı 5 stok adet 500 olan ürünü ekleyin
+
+            //T-SQL
+
+            //insert into Products (ProductName,CategoryID,UnitPrice,UnitsInStock) values ('Kola',1,5,500)
+
+            //Ekleme İşlemi
+            Product p = new Product();
+            p.ProductName = "Ayran";
+            p.UnitPrice = 5;
+            p.UnitsInStock = 500;
+            //int catID = db.Categories.First(x => x.CategoryName == "beverages").CategoryID; 
+            int catID2 = db.Categories.FirstOrDefault(x => x.CategoryName == "beverages").CategoryID;
+            p.CategoryID = catID2;
+
+            //db.Products.Add(new Product
+            // {
+            //     ProductName = "Kola",
+            //     UnitPrice = 5,
+            //     UnitsInStock = 500,
+            //     CategoryID = db.Categories.FirstOrDefault(x => x.CategoryName == "beverages").CategoryID
+            // });
+
+            db.Products.Add(p);
+            db.SaveChanges();
+        }
+
+        private void btnSorgu5_Click(object sender, EventArgs e)
+        {
+            // Fiyatı 20'den büyük veya 100'den küçük olan ürünlerin ilk 3'nü getiren sorgu.
+
+            dataGridView1.DataSource = db.Products
+                                    .Where(x => x.UnitPrice > 20 || x.UnitsInStock < 100)
+                                    .Select(x => new
+                                    {
+                                        x.ProductID,
+                                        x.ProductName,
+                                        x.UnitPrice,
+                                        x.UnitsInStock
+                                    })
+                                    .OrderBy(x => new
+                                    {
+                                        x.UnitPrice,
+                                        x.UnitsInStock
+                                    })
+                                    .Skip(3) //ilk üçünü geç
+                                    .Take(3) //ilk 3 nü getir (Skip olduğu için 2. üçlüyü getirecek..
+                                    .ToList();
+        }
+
+        private void btnSorgu6_Click(object sender, EventArgs e)
+        {
+            //Janet'in almış olduğu siparişleri listeleyin
+
+            //T-SQL
+
+            //select * from Orders Where EmployeeID = (Select EmployeeID from Employees Where FirstName = 'Janet')
+
+            //Linq
+
+            //var result = from o in db.Orders where o.Employee.FirstName == "Janet" select o;
+            //dataGridView1.DataSource = result.ToList();
+
+            //Linq to entity
+
+            dataGridView1.DataSource = db.Orders.Where(x => x.Employee.FirstName == "Janet").Select(x => new
+            {
+                x.OrderID,
+                Fullname = x.Employee.FirstName + " " + x.Employee.LastName,
+                x.OrderDate,
+                x.Freight,
+                x.ShipName
+            }).ToList();
+        }
+
+        private void btnSorgu7_Click(object sender, EventArgs e)
+        {
+            //ürün adında r ile başlayan t ile biten ürünler nelerdir.
+
+            //T-SQL
+
+            //Select * from Product Where ProductName like 'r%t'
+
+            //Linq
+
+            //var result = from p in db.Products
+            //             where p.ProductName.StartsWith("r") && p.ProductName.EndsWith("t")
+            //             select p;
+            //dataGridView1.DataSource = result.ToList();
+
+            //Linq to entity
+
+            dataGridView1.DataSource = db.Products.Where(x => x.ProductName.StartsWith("r") && x.ProductName.EndsWith("t")).Select(x => new
+            {
+                x.ProductName,
+                x.UnitPrice
+            }).OrderBy(x => x.UnitPrice).ToList();
+        }
+
+        private void btnSorgu8_Click(object sender, EventArgs e)
+        {
+            Product product = db.Products.Find(6);
+            if (product == null)
+                MessageBox.Show("dont have product");
+            else
+                MessageBox.Show($"Product Name: {product.ProductName}");
+        }
+
+        private void btnSorgu9_Click(object sender, EventArgs e)
+        {
+            //Hangi kategoriden stokta toplam ne kadar ürün var ?
+
+            //T-SQL
+
+            //select c.CategoryName,sum(p.UnitsInStock) from Products p join Categories c on p.CategoryID=c.CategoryID group by c.CategoryName            
+            //Linq
+
+            //var result = from p in db.Products
+            //             group p by p.Category.CategoryName into kyw
+            //             select new
+            //             {
+            //                 CategoryName = kyw.Key,
+            //                 TotalProduct = kyw.Sum(z => z.UnitsInStock)
+            //             };
+            //dataGridView1.DataSource = result.ToList();
+
+            //Linq To Entity
+
+            dataGridView1.DataSource = db.Products.GroupBy(x => x.Category.CategoryName).Select(x => new
+            {
+                Categoryname = x.Key,
+                TotalProduct = x.Sum(z => z.UnitsInStock)
+            }).ToList();
+        }
+
+        private void btnSorgu10_Click(object sender, EventArgs e)
+        {
+            //En pahalı ve en ucuz ürünler hangileri
+            decimal? expensiveProduct = db.Products.Max(x => x.UnitPrice);
+            decimal? cheaperProduct = db.Products.Min(x => x.UnitPrice);
+            MessageBox.Show($"Expensive Product : {expensiveProduct} \nCheaper Product : {cheaperProduct}");
+
+            //Ürünlerden en az stok olan en fazla stok olanların sayılarını getiren sorgu
+            int? leastStock = db.Products.Max(x => x.UnitsInStock);
+            int? mostStock = db.Products.Min(x => x.UnitsInStock);
+            MessageBox.Show($"LeastStock Quantity: {leastStock} \n MostStock Quantity: {mostStock}");
+        }
+
+        private void btnSorgu11_Click(object sender, EventArgs e)
+        {
+            //Fiyatı 200'den fazla olan ürünü getiren sorgu.
+
+            Product product = null;
+            try
+            {
+                product = db.Products.Single(x => x.UnitPrice > 200);
+                listBox1.Items.Add(product.ProductName);
+            }
+            catch (Exception)
+            {
+                if (product == null)
+                    MessageBox.Show("Don't have product");
+            }
+        }
+
+        private void btnSorgu12_Click(object sender, EventArgs e)
+        {
+            //Çalışanların ülkelerini listeleyin
+            dataGridView1.DataSource = db.Employees.GroupBy(x => x.Country).Select(x => new
+            {
+                country = x.Key,
+                Sayi = x.Count()
+            }).Distinct().ToList();
+        }
+
+        private void btnSorgu13_Click(object sender, EventArgs e)
+        {
+            //Hangi Müşteri toplam kaç adet sipariş vermiş.. Join example..
+
+            dataGridView1.DataSource = db.Orders
+                .Join(db.Customers, o => o.CustomerID, c => c.CustomerID, (o, c) => new { o, c })
+                .GroupBy(x => x.c.CompanyName)
+                .Select(x => new
+                {
+                    CompanyName = x.Key,
+                    Count = x.Count()
+                })
+                .OrderBy(x => x.Count)
+                .ToList();
+        }
+
+        private void btnSorgu14_Click(object sender, EventArgs e)
+        {
+            //Hangi Siparişten ne kadar gelir elde edilmiş ?
+
+            //var orderdetails = from od in db.Order_Details
+            //                   orderby od.Quantity * od.UnitPrice * (int)Math.Floor(1 - od.Discount)
+            //                   group od by od.OrderID into kyw
+            //                   select new
+            //                   {
+            //                       OrderId = kyw.Key,
+            //                       Total = kyw.Sum(x => x.Quantity * x.UnitPrice * (int)Math.Floor(1 - x.Discount))
+            //                   };
+            //dataGridView1.DataSource = orderdetails.ToList();
+
+            dataGridView1.DataSource = db.Order_Details.GroupBy(x => x.OrderID)
+                .Select(x => new
+                {
+                    OrderID = x.Key,
+                    TotalPrice = x.Sum(z => z.Quantity * z.UnitPrice * (int)Math.Floor(1 - z.Discount))
+                }).OrderByDescending(x => x.TotalPrice)
+                .ToList();
+        }
+    }
+}
